a5be15ea26be25b3fe01ad52ffcb0cb68ea800ac
Jonathan Christ <matthewc.calaycay@gmail.com>
2024-11-26 21:23:02 +0800

    style: added input highlight

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -13,7 +13,8 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
           "flex h-12 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           "min-w-[280px] sm:min-w-[320px] md:min-w-[400px] lg:min-w-[100px]",
           "bg-white text-black font-poppins rounded-[8px]",
-          "p-[12px] sm:p-[16px] md:p-[20px]",           
+          "p-[12px] sm:p-[16px] md:p-[20px]",
+          "focus-visible:ring-2 focus-visible:ring-primary",                  
           className
         )}
         ref={ref}
