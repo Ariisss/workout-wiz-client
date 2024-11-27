daaf3c84d3f0df92a790cefb57263b68cb97d76a
Jonathan Christ <matthewc.calaycay@gmail.com>
2024-11-27 10:57:15 +0800

    chore: removed unused window size handler code

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -6,24 +6,6 @@ import LoginForm from "@/components/auth/LoginForm";
 import Link from "next/link";
 
 export default function Login() {
-    const [screenSize, setScreenSize] = useState("sm");
-
-    useEffect(() => {
-        const updateScreenSize = () => {
-            if (window.innerWidth < 640) {
-                setScreenSize("sm");
-            } else if (window.innerWidth >= 640 && window.innerWidth < 1024) {
-                setScreenSize("md");
-            } else {
-                setScreenSize("lg");
-            }
-        };
-
-        updateScreenSize(); // Initialize on mount
-        window.addEventListener("resize", updateScreenSize); // Update on resize
-
-        return () => window.removeEventListener("resize", updateScreenSize);
-    }, []);
 
     const Content = (): React.ReactNode => (
         <div className="flex flex-col space-y-4">
