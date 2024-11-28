71fc9e0345b6170ee7fab0a5f6f0c5bcb8159bd9
Jonathan Christ <matthewc.calaycay@gmail.com>
2024-11-29 02:06:42 +0800

    refactor: migrated from tailwind-animated to framer motion

--- components/general/LockDiv.tsx
+++ components/general/LockDiv.tsx
@@ -1,25 +1,56 @@
 import clsx from "clsx"
 import { cn } from "@/lib/utils"
+import CircularRunes from "./CircularRunes"
+import { motion } from "motion/react"
 
 type LockDivProps = {
     lock: boolean
 }
 
 
-export function LockDiv({ lock }: LockDivProps) {
-    const stateStyle = clsx({ 'hidden': !lock, "hidden lg:block": lock })
+export function LockDiv() {
 
-    const animateStyle = {
-        base: 'animate-once delay-150 duration-1000',
-        horizontal: 'animate-fade-left',
-        vertical: 'animate-fade-up',
+    const springStyle = {
+        transition: {
+            type: 'spring',
+            mass: 0.5,
+            stiffness: 125,
+            damping: 8,
+            delay: 0.25
+        }
     }
 
     return (
-        <div className={cn(`absolute-center overflow-hidden select-none bg-background-darker opacity-90`,
-            `animate-fade animate-once animate-ease-out animate-alternate animate-fill-backwards`, stateStyle)}>
-            <div className="relative flex justify-center items-center text-[#153C2E]/60 text-[416px]">
-                <div className={cn('flex flex-row', animateStyle.base, animateStyle.horizontal)}>
+        <motion.div
+            className={cn(`absolute-center overflow-hidden select-none bg-background-darkest`, "hidden lg:block")}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 0.1 } }}
+            exit={{ opacity: 0, transition: { delay: 0.5, duration: 0.3 } }}
+            key={'l1'}
+        >
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: [1, 0.4] }}
+                exit={{ opacity: [0.4, 0] }}
+                transition={{ duration: 0.75, times: [0.2, 1] }}
+                key={'l2'}
+                className="animate-pulse animate-delay-[1s] opacity-40"
+            >
+                <CircularRunes
+                    parentHeight={350}
+                    fontSize={128}
+                    letterSpacing={0}
+                    glow="off"
+                />
+            </motion.div>
+            <div className="relative flex justify-center items-center text-primary-light text-[416px]">
+                <motion.div
+                    className={cn('flex flex-row')}
+                    initial={{ x: '-100%' }}
+                    animate={{ x: '0%', ...springStyle }}
+                    exit={{ x: '-100%', ...springStyle }}
+                    key={'l3'}
+                >
                     {"ᛝᛝᛝ".split("").map((char, index) => (
                         <span key={`horizontal-${index}`} className='rotate-90 p-[50px]'>
                             <p
@@ -29,8 +60,14 @@ export function LockDiv({ lock }: LockDivProps) {
                             </p>
                         </span>
                     ))}
-                </div>
-                <div className={cn("flex flex-col absolute", animateStyle.base, animateStyle.vertical)}>
+                </motion.div>
+                <motion.div
+                    className={cn('flex flex-col absolute')}
+                    initial={{ y: '-100%' }}
+                    animate={{ y: '0px', ...springStyle }}
+                    exit={{ y: '-100%', ...springStyle }}
+                    key={'l4'}
+                >
                     {"ᛝᛝᛝ".split("").map((char, index) => (
                         <span key={`vertical-${index}`} className="h-[340px] flex items-center">
                             <p
@@ -40,8 +77,8 @@ export function LockDiv({ lock }: LockDivProps) {
                             </p>
                         </span>
                     ))}
-                </div>
-            </div>
-        </div >
+                </motion.div>
+            </div >
+        </motion.div >
     )
 }
