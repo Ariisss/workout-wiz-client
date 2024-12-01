e1690f848e29b2f1e999a72f51572cc9ee1422ac
Jonathan Christ <matthewc.calaycay@gmail.com>
2024-12-02 01:15:16 +0800

    feat: added sidebar placeholder

---
+++ components/general/navigation/DesktopSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+export default function DesktopSidebar({ }: Props) {
+    return (
+        <div className='w-[250px]'>
+            DesktopSidebar
+        </div>
+    )
+}
