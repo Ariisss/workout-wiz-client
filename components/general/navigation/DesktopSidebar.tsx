1c3099afd1ee8f98e5552ae69e0dc1fb10e5c82c
Jonathan Christ <matthewc.calaycay@gmail.com>
2024-12-03 04:42:43 +0800

    feat: added desktop sidebar

--- components/general/navigation/DesktopSidebar.tsx
+++ components/general/navigation/DesktopSidebar.tsx
@@ -1,11 +1,142 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
+import {
+    LayoutDashboard,
+    ChartNoAxesColumn,
+    Dumbbell,
+    ScrollText,
+    Settings,
+    LogOut,
+    Toilet //CHANGE THIS TO ACTUAL ICON LATER PLS
+} from "lucide-react";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 type Props = {}
 
 export default function DesktopSidebar({ }: Props) {
+    const links = [
+        {
+            label: "Dashboard",
+            href: "/dashboard",
+            icon: (
+                <LayoutDashboard id="icon" className="text-muted-foreground h-5 w-5 flex-shrink-0" />
+            ),
+        },
+        {
+            label: "Plans",
+            href: "/plans",
+            icon: (
+                <Dumbbell id="icon" className="text-muted-foreground h-5 w-5 flex-shrink-0" />
+            ),
+        },
+        {
+            label: "Logs",
+            href: "/logs",
+            icon: (
+                <ScrollText id="icon" className="text-muted-foreground h-5 w-5 flex-shrink-0" />
+            ),
+        },
+        {
+            label: "Progress",
+            href: "/progress",
+            icon: (
+                <ChartNoAxesColumn id="icon" className="text-muted-foreground h-5 w-5 flex-shrink-0" />
+            ),
+        },
+        {
+            label: "Settings",
+            href: "/settings",
+            icon: (
+                <Settings id="icon" className="text-muted-foreground h-5 w-5 flex-shrink-0" />
+            ),
+        }
+    ];
+    const [open, setOpen] = useState(false);
+    const pathname = usePathname()
+
     return (
-        <div className='w-[250px]'>
-            DesktopSidebar
+        <div
+            className={cn(
+                "rounded-md flex flex-col md:flex-row bg-background-darkest w-full flex-1 max-w-7xl mx-auto overflow-hidden",
+                "h-screen"
+            )}
+        >
+            <Sidebar open={open} setOpen={setOpen}>
+                <SidebarBody className="justify-between gap-12">
+                    <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+                        {open ? <Logo /> : <LogoIcon />}
+                        <div className="mt-10 flex flex-col gap-[16px]">
+                            {links.map((link, idx) => {
+                                const iconClass = cn(
+                                    '[&_#label]:text-primary-light [&_#icon]:text-primary-light',
+                                    '[&_#icon]:fill-primary-light')
+                                return (
+                                    <SidebarLink
+                                        key={idx}
+                                        link={link}
+                                        className={clsx(
+                                            (pathname === link.href) && iconClass
+                                        )}
+                                    />
+                                )
+                            })}
+                        </div>
+                    </div>
+                    <div>
+                        <SidebarLink
+                            link={{
+                                label: "Logout",
+                                href: "#",
+                                icon: <LogoutIcon />
+                            }}
+                        />
+                    </div>
+                </SidebarBody>
+            </Sidebar>
         </div>
+    );
+}
+
+const Logo = () => {
+    return (
+        <Link
+            href="#"
+            className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+        >
+            <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+            <motion.span
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                className="font-medium text-black dark:text-white whitespace-pre"
+            >
+                Workout Wizard
+            </motion.span>
+        </Link>
+    )
+}
+
+const LogoIcon = () => {
+    return (
+        <Link
+            href="#"
+            className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+        >
+            <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+        </Link>
+    )
+}
+
+const LogoutIcon = () => {
+    return (
+        <Link
+            href="#"
+            className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+        >
+            <LogOut id="logo" className="text-neutral-200 h-5 w-5" />
+        </Link>
     )
 }
--- components/general/navigation/Navigation.tsx
+++ components/general/navigation/Navigation.tsx
@@ -13,18 +13,20 @@ type Props = {
 export default function NavLayout({ children }: Props) {
     const pathname = usePathname()
     return (
-        <div className='flex flex-col md:flex-row h-full font-sans'>
-            <div className='h-full'>
+        <div className='flex flex-col md:flex-row h-full font-sans gap-8'>
+            <div className='flex-1 h-full order-1 md:order-2'>
                 {children}
             </div>
             <div className={clsx(
-                "w-screen md:w-fit h-fit",
+                "flex flex-col md:flex-row",
+                "md:w-fit w-full",
+                "order-2 md:order-1",
                 { "hidden": (publicRoutes.includes(pathname)) }
             )}>
-                <div className='md:hidden p-0 m-0'>
+                <div className='md:hidden p-0 m-0 fixed bottom-0 left-0 right-0'>
                     <MobileNavbar />
                 </div>
-                <div className='hidden md:flex'>
+                <div className='hidden md:flex h-full'>
                     <DesktopSidebar />
                 </div>
             </div>
