72b75538436978d34949e30a8c5d3a6f3ee7b07a
Prince Isaac Pantino <arispantino@gmail.com>
2024-12-06 18:33:08 +0800

    feat: added signup data context in form provider

--- components/context/FormProvider.tsx
+++ components/context/FormProvider.tsx
@@ -8,17 +8,39 @@ type FormState = {
     }
 }
 
+interface SignupData {
+    email?: string
+    password?: string
+    username?: string
+    dob?: string
+    sex?: boolean
+    height?: number
+    weight?: number
+    goal_type?: string[]
+    with_gym?: boolean
+    workout_days?: string[]
+    intensity?: string
+}
+
 type FormContextProps = {
     forms: FormState
     lockForm: (formId: string) => void
     unlockForm: (formId: string) => void
     submitForm: (formId: string) => void
+    signupData: SignupData
+    updateSignupData: (data: Partial<SignupData>) => void
 }
+
 
 const FormContext = createContext<FormContextProps | null>(null);
 
 export const FormProvider = ({ children }: React.PropsWithChildren) => {
     const [forms, setForms] = useState<FormState>({});
+    const [signupData, setSignupData] = useState<SignupData>({});
+
+    const updateSignupData = (data: Partial<SignupData>) => {
+        setSignupData((prev) => ({ ...prev, ...data }));
+    }
 
     const lockForm = (formId: string) => {
         setForms((prev) => ({
@@ -43,7 +65,7 @@ export const FormProvider = ({ children }: React.PropsWithChildren) => {
 
 
     return (
-        <FormContext.Provider value={{ forms, lockForm, unlockForm, submitForm }}>
+        <FormContext.Provider value={{ forms, lockForm, unlockForm, submitForm, signupData, updateSignupData }}>
             {children}
         </FormContext.Provider>
     );
@@ -55,7 +77,7 @@ export const useFormContext = (formId: string) => {
         throw new Error("useFormContext must be used within a FormProvider");
     }
 
-    const { forms, lockForm, unlockForm, submitForm } = context;
+    const { forms, lockForm, unlockForm, submitForm, signupData, updateSignupData } = context;
     const formState = forms[formId] || { isLocked: false, isSubmitted: false };
 
     return {
@@ -63,5 +85,7 @@ export const useFormContext = (formId: string) => {
         lockForm: () => lockForm(formId),
         unlockForm: () => unlockForm(formId),
         submitForm: () => submitForm(formId),
+        signupData,
+        updateSignupData
     }
 };
