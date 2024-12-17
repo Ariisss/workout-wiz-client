421e8b081a8474e66c465e4b7ee5f8ffe8868ce6
kulaizki <kulaizke@gmail.com>
2024-12-17 21:27:00 +0800

    refactor: update page title formats

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import React from "react";
 
 export const metadata: Metadata = {
-    title: "Settings",
+    title: "Wiz | Settings",
 };
 
 type SettingsLayoutProps = {
